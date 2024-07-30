4ae731b7aafd1759bcedb598017d2e144e7a0635
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-07-30 11:51:16 +0200

    template for cart done and working on addEventListener

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,24 @@
 import './../scss/style.scss';
 
+// Hamburger Navigation //
+
+const burgerMenu = document.getElementById('hamburger-menu-icon');
+
+const overlay = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function () {
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+});
+
+
+// CART //
+
+const productCard = document.querySelectorAll('[data-product-card]');
+const cart = document.getElementById('cart');
+
+console.log(productCard, cart);
+
+addEventListener.productCard('click', function(){
+    const clone = document.importNode(template.content, true)
+})
