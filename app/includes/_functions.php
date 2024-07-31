341be21adb4389894b99eb24222297ff6e10adf2
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-07-31 15:11:10 +0200

    repaired everything

--- app/includes/_functions.php
+++ app/includes/_functions.php
@@ -41,10 +41,14 @@ function getAllProducts(PDO $dbCo)
        echo '
         <li class="product__card">
             <button data-product-card="' . $product["id_product"] . '" data-product-name="' . $product["product_name"] . '" data-product-price="' . $product["price"] . '" data-product-image="' . $product["image_url"] . '">
-                <img class="product__img" src="' . $product["image_url"] . '" alt="">
-                <h3>' . $product["product_name"] . '</h3>
-                <p class="product__price">' . $product["price"] . '<img src="./img/coin.svg" alt="pièce d\'or"></p>
-                <p>' . $product["stock"] . '</p>
+            <img class="product__card-img" src="' . $product["image_url"] . '" alt="">
+                <div class="product__card-infos">
+                    <h3>' . $product["product_name"] . '</h3>
+                    <div class="product__infosPrice">
+                        <p class="product__price">' . $product["price"] . '<img src="./img/coin.svg" alt="pièce d\'or"></p>
+                        <p>' . $product["stock"] . '</p>
+                    </div>
+                </div>
             </button>
         </li>';
     }
